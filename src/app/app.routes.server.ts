558b9108c8027273021f62e0f0f3d7959334fbbe
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-02-15 15:14:55 +0530

    dynamic route added

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  //  Dynamic category route → server render
+  {
+    path: 'product-category/:categorySlug',
+    renderMode: RenderMode.Server,
+  },
+
+  // All other pages → prerender
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
